c34b6ad3bbb69cf5f6552579636282fd89b72dc0
JosephJasin <josef20606@gmail.com>
2021-01-02 20:18:20 +0200

    initalize firebase

---
+++ lib/routes/homeScreen.dart
@@ -0,0 +1,16 @@
+import 'package:flare/widgets.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
